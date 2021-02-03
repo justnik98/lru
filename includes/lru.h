caf009b1eb4e29e1ad3da6b2aa903e38dd503592
Nikita Yankovskii <yannikita98@gmail.com>
2021-02-03 22:34:50 +0300

    optimication for put and has methods

--- includes/lru.h
+++ includes/lru.h
@@ -8,11 +8,12 @@
 #include <cstddef>
 #include <list>
 #include <unordered_map>
+
 template<class T>
 class lru {
 private:
-    std::list<T> list_;
-    std::unordered_map<T, typename std::list<T>::iterator> map_;
+    std::list<T> list_{};
+    std::unordered_map<T, typename std::list<T>::iterator> map_{};
     std::size_t max_size;
 public:
     lru(const std::size_t &maxSize);
@@ -31,8 +32,7 @@ bool lru<T>::has(const T &key) {
     if (it == map_.end()) return false;
 
     auto list_it = it->second;
-    list_.erase(list_it);
-    it->second = list_.insert(list_.end(), key);
+    list_.splice(list_.end(), list_, list_it);
     return true;
 }
 
@@ -40,15 +40,17 @@ template<class T>
 bool lru<T>::put(const T &key) {
     auto it = map_.find(key);
     if (it != map_.end()) {
-        list_.erase(it->second);
-        it->second = list_.insert(list_.end(), key);
+        auto list_it = it->second;
+        list_.splice(list_.end(), list_, list_it);
         return false;
     }
     if (list_.size() == max_size) {
         T last = list_.front();
         list_.pop_front();
         map_.erase(last);
-        map_[key] = list_.insert(list_.end(), key);
+        list_.front() = key;
+        list_.splice(list_.end(), list_, list_.begin());
+        map_[key] = --list_.end();
     } else {
         map_[key] = list_.insert(list_.end(), key);
     }
