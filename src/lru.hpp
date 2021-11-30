2360fe781d082ee99d2a1629a4c6cdf191f93770
Nikita Yankovskii <yannik98@yandex.ru>
2021-12-01 02:12:37 +0300

    added intrusive list

--- src/lru.hpp
+++ src/lru.hpp
@@ -8,12 +8,27 @@
 #include <list>
 #include <unordered_map>
 
+#include <boost/intrusive/link_mode.hpp>
+#include <boost/intrusive/list.hpp>
+#include <boost/intrusive/list_hook.hpp>
+
+using LinkMode = boost::intrusive::link_mode<boost::intrusive::normal_link>;
+using LruHook = boost::intrusive::list_base_hook<LinkMode>;
+
+class LruNode final : public LruHook {
+};
+
 template<class T>
 class lru {
 private:
-    std::list<T> list_;
-    std::unordered_map<T, typename std::list<T>::iterator> map_;
+    using List = boost::intrusive::list<LruNode, boost::intrusive::constant_time_size<false>>;
+    using Map = std::unordered_map<T, LruNode>;
+    List list_;
+    Map map_;
     std::size_t max_size_;
+
+    const T &getLeastRecentKey();
+
 public:
     explicit lru(size_t maxSize) : max_size_(maxSize) {};
 
@@ -28,8 +43,7 @@ bool lru<T>::has(const T &key) {
     if (it == map_.end()) {
         return false;
     }
-    auto list_it = it->second;
-    list_.splice(list_.end(), list_, list_it);
+    list_.splice(list_.end(), list_, list_.iterator_to(it->second));
     return true;
 }
 
@@ -37,23 +51,27 @@ template<class T>
 bool lru<T>::put(const T &key) {
     auto it = map_.find(key);
     if (it != map_.end()) {
-        auto list_it = it->second;
-        list_.erase(list_it);
-        it->second = list_.insert(list_.end(), key);
+        list_.splice(list_.end(), list_, list_.iterator_to(it->second));
         return false;
     }
-    if (list_.size() == max_size_) {
-        T last = list_.front();
-        auto node = map_.extract(last);
-        list_.front() = key;
+    if (map_.size() == max_size_) {
+        auto node = map_.extract(getLeastRecentKey());
         list_.splice(list_.end(), list_, list_.begin());
         node.key() = key;
-        node.mapped() = --list_.end();
         map_.insert(std::move(node));
     } else {
-        map_[key] = list_.insert(list_.end(), key);
+        auto[it, ok] = map_.emplace(key, LruNode{});
+        assert(ok);
+        list_.insert(list_.end(), it->second);
     }
     return true;
+}
+
+template<class T>
+const T &lru<T>::getLeastRecentKey() {
+    using Pair = typename Map::value_type;
+    constexpr auto offset = offsetof(Pair, second) - offsetof(Pair, first);
+    return *reinterpret_cast<const T *>(reinterpret_cast<const char *>(&list_.front()) - offset);
 }
 
 
